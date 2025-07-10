4cfe5ec1fec917953892487ce84f100874b7c77e
acolxon <asylkhantleulin2004@gmail.com>
2025-07-10 22:18:16 +0500

    connect.php / changes data for connect to database

--- connect.php
+++ connect.php
@@ -1,19 +1,19 @@
 <?php
-$localhost = "localhost";
-$dbname = "courses";
-$login = "root";
-$pass = "";
-$charset = "utf8mb4";
+    $localhost = "localhost";
+    $dbname = "courses";
+    $login = "p-347186_asylkhan";
+    $pass = "asylkhan04";
+    $charset = "utf8mb4";
 
-$dsn = "mysql:host=$localhost;dbname=$dbname;charset=$charset";
+    $dsn = "mysql:host=$localhost;dbname=$dbname;charset=$charset";
 
-try {
-    $pdo = new PDO($dsn, $login, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Ошибки — через исключения
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // По умолчанию массивы
-        PDO::ATTR_EMULATE_PREPARES => false, // Настоящие prepare-запросы
-    ]);
-} catch (PDOException $e) {
-    die("Ошибка подключения к базе: " . $e->getMessage()); // Показывать только в dev!
-}
+    try {
+        $pdo = new PDO($dsn, $login, $pass, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Ошибки — через исключения
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // По умолчанию массивы
+            PDO::ATTR_EMULATE_PREPARES => false, // Настоящие prepare-запросы
+        ]);
+    } catch (PDOException $e) {
+        die("Ошибка подключения к базе: " . $e->getMessage()); // Показывать только в dev!
+    }
 ?>
